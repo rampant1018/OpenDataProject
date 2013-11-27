99bc090c63fdfd4dcf9dcf78c97395155d7444bd
a020304888a <a020304888a@gmail.com>
2013-11-27 21:09:32 +0800

    Fetch
    Signed-off-by: a020304888a <a020304888a@gmail.com>

--- OpenProject/src/Main.java
+++ OpenProject/src/Main.java
@@ -1,3 +1,5 @@
+import java.net.*;
+import java.io.*;
 
 public class Main {
 
@@ -6,7 +8,11 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//String web = "http://comcat.cr.usgs.gov/fdsnws/event/1/query?starttime=2013-09-05&endtime=2013-09-06&format=geojson";
+		
+		String temp;
+		temp = Fetch.getJson("2013-09-05","2013-09-06");
+		System.out.println(temp);
 	}
 
 }
