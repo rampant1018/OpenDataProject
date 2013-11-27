13beef8af7993259a01ac681c53d74d5ce46935f
rampant <rampant1018@gmail.com>
2013-11-27 23:23:17 +0800

    Update Main.java

--- OpenProject/src/Main.java
+++ OpenProject/src/Main.java
@@ -7,7 +7,8 @@ public class Main {
                 // TODO Auto-generated method stub
                 Parser parser = new Parser();
                 Fetch fetch = new Fetch();
-                parser.test(fetch.getJson("2012-09-05", "2012-09-06"));
+                //parser.test(fetch.getJson("2012-09-05", "2012-09-06"));
+                parser.test();
         }
         
         
