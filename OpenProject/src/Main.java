b23c7a7a493b3d8f1ae91258ccffbfffa628660b
rampant <rampant1018@gmail.com>
2013-11-27 21:49:05 +0800

    Add parse function - getEventList Read mag, location, time, longitude and latitude from given json content and put all dataset(HashMap) into eventList.
    Modify test function
    Read json testfile and call getEventList function
    Display all dataset in console.

--- OpenProject/src/Main.java
+++ OpenProject/src/Main.java
@@ -1,5 +1,3 @@
-import java.net.*;
-import java.io.*;
 
 public class Main {
 
@@ -8,11 +6,9 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//String web = "http://comcat.cr.usgs.gov/fdsnws/event/1/query?starttime=2013-09-05&endtime=2013-09-06&format=geojson";
-		
-		String temp;
-		temp = Fetch.getJson("2013-09-05","2013-09-06");
-		System.out.println(temp);
+		Parser parser = new Parser();
+		parser.test();
 	}
-
+	
+	
 }
--- OpenProject/src/Parser.java
+++ OpenProject/src/Parser.java
@@ -4,14 +4,26 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.json.*;
 
 public class Parser {
+	List<Map<String, String>> eventList = new ArrayList();;
+	
 	public void test() {
 		try {
 			String content = readFile("testfile/query.txt", StandardCharsets.UTF_8);
-			System.out.println(content);
-		} catch (IOException e) {
+			getEventList(content);
+			
+			for(int i = 0; i < eventList.size(); i++) {
+				System.out.println("震度： " + eventList.get(i).get("mag") + ", 地點： " + eventList.get(i).get("location") + ", 時間： " + eventList.get(i).get("time") + ", 經度： " + eventList.get(i).get("longitude") + ", 緯度： " + eventList.get(i).get("latitude"));
+			}
+			
+		} catch (IOException | JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -20,5 +32,32 @@ public class Parser {
 	private String readFile(String path, Charset encoding) throws IOException {
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
 		return encoding.decode(ByteBuffer.wrap(encoded)).toString();
+	}
+	
+	private void getEventList(String content) throws JSONException {
+		JSONObject jsonObj = new JSONObject(new JSONTokener(content));
+		JSONArray eventArray = jsonObj.getJSONArray("features");
+		
+		for(int i = 0; i < eventArray.length(); i++) {
+			JSONObject event = eventArray.getJSONObject(i);
+			
+			Double mag, longitude, latitude;
+			Long time;
+			String location;
+			mag = event.getJSONObject("properties").getDouble("mag");
+			location = event.getJSONObject("properties").getString("place");
+			time = event.getJSONObject("properties").getLong("time");
+			longitude = event.getJSONObject("geometry").getJSONArray("coordinates").getDouble(0);
+			latitude = event.getJSONObject("geometry").getJSONArray("coordinates").getDouble(1);
+			
+			HashMap<String, String>eventDetail = new HashMap<String, String>();
+			eventDetail.put("mag", mag.toString());
+			eventDetail.put("location", location);
+			eventDetail.put("time", time.toString());
+			eventDetail.put("longitude", longitude.toString());
+			eventDetail.put("latitude", latitude.toString());
+			
+			eventList.add(eventDetail);
+		}
 	}
 }
