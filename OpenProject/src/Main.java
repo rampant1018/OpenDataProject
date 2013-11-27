8b2e0fa37ef72bdfcd96cd79cd23634ccb361b3a
rampant <rampant1018@gmail.com>
2013-11-27 23:41:08 +0800

    fix bugs

--- OpenProject/src/Main.java
+++ OpenProject/src/Main.java
@@ -6,8 +6,7 @@ public class Main {
         public static void main(String[] args) {
                 // TODO Auto-generated method stub
                 Parser parser = ProjectFactory.getParser();
-                Fetch fetch = ProjectFactory.getFetch();
-                parser.test(fetch.getJson("2012-09-05", "2012-09-06"));
+                parser.test();
         }
         
         
