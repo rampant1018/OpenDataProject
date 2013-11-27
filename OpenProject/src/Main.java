cd601ec8740192a45c7e19c36d080f6946a6a16a
a020304888a <a020304888a@gmail.com>
2013-11-27 23:18:51 +0800

    add comment and factory
    Signed-off-by: a020304888a <a020304888a@gmail.com>

--- OpenProject/src/Main.java
+++ OpenProject/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
          */
         public static void main(String[] args) {
                 // TODO Auto-generated method stub
-                Parser parser = new Parser();
-                Fetch fetch = new Fetch();
+                Parser parser = ProjectFactory.getParser();
+                Fetch fetch = ProjectFactory.getFetch();
                 parser.test(fetch.getJson("2012-09-05", "2012-09-06"));
         }
         
