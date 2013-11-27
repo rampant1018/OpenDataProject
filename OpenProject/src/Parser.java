f47f719d5faaadcd6c5905b7b23e08222db0ae7d
rampant <rampant1018@gmail.com>
2013-11-27 20:53:19 +0800

    Add test function - readFile used for testing json file in Parser class

--- OpenProject/src/Parser.java
+++ OpenProject/src/Parser.java
@@ -1,4 +1,24 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class Parser {
-
+	public void test() {
+		try {
+			String content = readFile("testfile/query.txt", StandardCharsets.UTF_8);
+			System.out.println(content);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return encoding.decode(ByteBuffer.wrap(encoded)).toString();
+	}
 }
