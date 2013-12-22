d61735b139eaaee091b2a96182180087919e0b0a
rampant <rampant1018@gmail.com>
2013-12-22 23:48:32 +0800

    Add GenerateMapStat function. Using google map API generate jpg file which contain Pacific Ocean satellite and predefined marker location.

--- OpenProject/src/OPEarth/Chart.java
+++ OpenProject/src/OPEarth/Chart.java
@@ -1,11 +1,21 @@
 package OPEarth;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
@@ -13,6 +23,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.data.time.Month;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
+import org.omg.CORBA_2_3.portable.OutputStream;
 
 public class Chart {
 	public void GenerateTimeSeriesChart(Map<Date, Integer> statData) {
@@ -36,5 +47,22 @@ public class Chart {
 		} catch(IOException e) {
 			System.err.println("Problem occurres creating chart.");
 		}
+	}
+	
+	public void GenerateMapStat(List<String> longitude, List<String> latitude) throws IOException {
+		String generateURL = "http://maps.googleapis.com/maps/api/staticmap?center=17.12207,179.39887&zoom=2&size=640x640&sensor=false&maptype=satellite&format=jpg&markers=";
+		
+		for(int i = 0; i < longitude.size(); i++) {
+			if(i != 0) {
+				generateURL += "%7C";
+			}
+			generateURL += longitude.get(i);
+			generateURL += ",";
+			generateURL += latitude.get(i);
+		}
+		
+		URL url = new URL(generateURL);
+		Image image = ImageIO.read(url);
+		ImageIO.write((RenderedImage) image, "jpg", new File("Map.jpg"));
 	}
 }
--- OpenProject/src/OPEarth/Main.java
+++ OpenProject/src/OPEarth/Main.java
@@ -1,7 +1,10 @@
 package OPEarth;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -14,6 +17,7 @@ public class Main {
                 Parser parser = ProjectFactory.getParser();
                 parser.test();
                 */
+        	/*
         	Map<Date, Integer> map = new HashMap<Date, Integer>();
         	map.put(new Date(1356998400000L), 520);
         	map.put(new Date(1359676800000L), 129);
@@ -22,9 +26,31 @@ public class Main {
         	map.put(new Date(1370044800000L), 11);
         	map.put(new Date(1372636800000L), 2249);
         	map.put(new Date(1375315200000L), 35);
-        	map.put(new Date(1377993600000L), 600);
+        	map.put(new Date(1377993600000L), 600);*/
         	Chart chart = new Chart();
-        	chart.GenerateTimeSeriesChart(map);
+        	//chart.GenerateTimeSeriesChart(map);
+        	
+        	List<String> longitude = new ArrayList<String>();
+        	List<String> latitude = new ArrayList<String>();
+        	longitude.add("14.29891");
+        	latitude.add("145.2153");
+        	longitude.add("30.21218");
+        	latitude.add("-179.6285");
+        	longitude.add("14.9454");
+        	latitude.add("-160.17769");
+        	longitude.add("39.16982");
+        	latitude.add("-147.6754");
+        	longitude.add("50.59983");
+        	latitude.add("-174.21519");
+        	longitude.add("49.46012");
+        	latitude.add("155.24419");
+        	
+        	try {
+				chart.GenerateMapStat(longitude, latitude);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
         
         
