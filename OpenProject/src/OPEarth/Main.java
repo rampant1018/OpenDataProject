ea3dda47e73e768c91be069eb0eb387137511b45
rampant <rampant1018@gmail.com>
2014-01-02 10:55:37 +0800

    Combine parser and postgresql database

--- OpenProject/src/OPEarth/Main.java
+++ OpenProject/src/OPEarth/Main.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import OPEarth.PostgreSQL.Entry;
+
 public class Main {
         /**
          * @param args
@@ -19,6 +21,18 @@ public class Main {
         	System.out.println("Main Thread");
         	Parser parser = ProjectFactory.getParser();
         	eventList = parser.parse();
+        	
+        	PostgreSQL database = new PostgreSQL();
+        	List<PostgreSQL.Entry> entryList = new ArrayList<PostgreSQL.Entry>();
+        	int count = 0;
+        	for(Map<String, String> event : eventList) {
+        		entryList.add(database.new Entry(Float.parseFloat(event.get("longitude")), Float.parseFloat(event.get("latitude")), Float.parseFloat(event.get("mag")), new Date(Long.parseLong(event.get("time"))), event.get("location").replace('\'', '`')));
+        		if(count == 200) {
+        			break;
+        		}
+        		count++;
+        	}
+        	database.InsertData(entryList);
         }
         
         
