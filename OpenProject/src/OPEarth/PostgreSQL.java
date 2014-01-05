049ccaf7ff9516fc3d848b2b5b6a5e222bfe3e6b
rampant <rampant1018@gmail.com>
2014-01-05 19:47:20 +0800

    Update javadoc

--- OpenProject/src/OPEarth/PostgreSQL.java
+++ OpenProject/src/OPEarth/PostgreSQL.java
@@ -9,7 +9,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/** API which is used to communicate with PostgreSQL database
+ * 
+ * @author rampant
+ *
+ */
 public class PostgreSQL {
+	/**
+	 * Insert earthquake info into database
+	 * @param entryList the data which need to be inserted
+	 */
 	public void InsertData(List<Entry> entryList) {
 		int lastID = 0;
 		String sql;
@@ -17,18 +26,21 @@ public class PostgreSQL {
 		try {
 			Class.forName("org.postgresql.Driver").newInstance();
 			
+			// Connection settings
 			String url="jdbc:postgresql://210.61.10.89:9999/Team2";
 			Connection con = DriverManager.getConnection(url,"Team2","lab2080362");
-			Statement st = con.createStatement();
 			
+			Statement st = con.createStatement();
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 			
+			// Get the last id from table
 			sql = "SELECT MAX(id) FROM earthquakeinfo;";
 			ResultSet rs = st.executeQuery(sql);
 			while(rs.next()) {
 				lastID = rs.getInt(1);
 			}
 			
+			// Insert every info into table. Manually control id value.
 			for(Entry entry : entryList) {
 				sql = "INSERT INTO earthquakeinfo VALUES(" + (lastID + 1) + ", " + entry.longitude + ", " + entry.latitude + ", " + entry.magnitude + ", '" + formatter.format(entry.time) + "', " + "'" + entry.location + "');";
 				lastID++;
@@ -36,15 +48,21 @@ public class PostgreSQL {
 				st.execute(sql);
 			}
 			
+			// Close connection
 			st.close();
 			con.close();
-			
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
 		}
 	}
 	
+	/** Earthquake info data format. Contain 'longitude', 'latitude', 'magnitude', 'time', 'location'
+	 * 
+	 * @author rampant
+	 *
+	 */
 	public class Entry {
 		float longitude;
 		float latitude;
@@ -52,6 +70,14 @@ public class PostgreSQL {
 		Date time;
 		String location;
 		
+		/** 
+		 * 
+		 * @param longitude
+		 * @param latitude
+		 * @param magnitude 
+		 * @param time timestamp with UNIX timestamp format
+		 * @param location description of the location where the earthquake occur
+		 */
 		public Entry(float longitude, float latitude, float magnitude, Date time, String location) {
 			this.longitude = longitude;
 			this.latitude = latitude;
--- OpenProject/src/OPEarth/ProjectFactory.java
+++ OpenProject/src/OPEarth/ProjectFactory.java
@@ -1,12 +1,48 @@
 package OPEarth;
 
+/** Try to use 'Simple Factory' design pattern. Used to generate instance.
+ * 
+ * @author rampant
+ *
+ */
 public class ProjectFactory {
-	public static Fetch getFetch(){
+	/** Get instance of Fetch
+	 * 
+	 * @return instance of Fetch
+	 */
+	public static Fetch getFetch() {
 		Fetch fetch = new Fetch();
+		
 		return fetch;
 	}
-	public static Parser getParser(){
+	
+	/** Get instance of Parser
+	 * 
+	 * @return instance of Parser
+	 */
+	public static Parser getParser() {
 		Parser parser = new Parser();
+		
 		return parser;
+	}
+	
+	/** Get instance of PostgreSQL
+	 * 
+	 * @return instance of PostgreSQL
+	 */
+	public static PostgreSQL getDatabase() {
+		PostgreSQL database = new PostgreSQL();
+		
+		return database;
+	}
+	
+	/** Get instance of Chart
+	 * 
+	 * @return instance of Chart
+	 */
+	public static Chart getChart() {
+		Chart chart = new Chart();
+		
+		return chart;
 	}
 }
