2db9b5b7810bafc96d9826182481c3e6a58f75d2
rampant <rampant1018@gmail.com>
2014-01-08 14:13:05 +0800

    Modify JUnit test

--- OpenProject/src/JUnit/ChartTest.java
+++ OpenProject/src/JUnit/ChartTest.java
@@ -9,6 +9,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.jfree.data.time.Month;
+import org.jfree.data.time.Year;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,15 +34,16 @@ public class ChartTest {
 	@Test
 	public void testGenTimeSeriesChart() {
 		boolean successful = true;
-		Map<Date, Integer> map = new HashMap<Date, Integer>();
-    	map.put(new Date(1356998400000L), 520);
-    	map.put(new Date(1359676800000L), 129);
-    	map.put(new Date(1362096000000L), 3029);
-    	map.put(new Date(1364774400000L), 2039);
-    	map.put(new Date(1370044800000L), 11);
-    	map.put(new Date(1372636800000L), 324);
-    	map.put(new Date(1375315200000L), 35);
-    	map.put(new Date(1377993600000L), 600);
+		Map<Year, Integer> map = new HashMap<Year, Integer>();
+    	map.put(new Year(2013), 520);
+    	map.put(new Year(2012), 302);
+    	map.put(new Year(2011), 132);
+    	map.put(new Year(2010), 3456);
+    	map.put(new Year(2009), 1342);
+    	map.put(new Year(2008), 1672);
+    	map.put(new Year(2007), 3333);
+    	map.put(new Year(2006), 5382);
+    	map.put(new Year(2005), 238);
     	
     	try {
     		chart.GenerateTimeSeriesChart(map);
@@ -54,38 +57,14 @@ public class ChartTest {
 	}
 	
 	@Test
-	public void testGenMapStat() {
-		boolean successful = true;
-		List<String> longitude = new ArrayList<String>();
-    	List<String> latitude = new ArrayList<String>();
-    	ArrayList<Integer> magnitude = new ArrayList<Integer>();
-    	longitude.add("14.29891");
-    	latitude.add("145.2153");
-    	magnitude.add(5);
-    	longitude.add("30.21218");
-    	latitude.add("-179.6285");
-    	magnitude.add(4);
-    	longitude.add("14.9454");
-    	latitude.add("-160.17769");
-    	magnitude.add(3);
-    	longitude.add("39.16982");
-    	latitude.add("-147.6754");
-    	magnitude.add(4);
-    	longitude.add("50.59983");
-    	latitude.add("-174.21519");
-    	magnitude.add(5);
-    	longitude.add("49.46012");
-    	latitude.add("155.24419");
-    	magnitude.add(4);
-    	
-    	try {
-			chart.GenerateMapStat(longitude, latitude, magnitude);
+	public void testGenerateMap() {
+		/*
+		try {
+			//chart.GenerateMap();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			successful = false;
 		}
-    	
-    	assertEquals(true, successful);
+		*/
 	}
 }
